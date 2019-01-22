0d0bc7b89946ebe4c770d569767d035188bc7562
paypol.af <s.developer@paypol.af>
2019-01-22 20:45:18 +0430

    home Activity and login view created

--- app/src/main/java/com/example/mahdi/ah_rental/HomeActivity.java
+++ app/src/main/java/com/example/mahdi/ah_rental/HomeActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -18,16 +19,20 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+//                    mTextMessage.setText(R.string.title_home);
+                    Toast.makeText(HomeActivity.this, "Click Home", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_share:
-                    mTextMessage.setText("shareproparty");
+//                    mTextMessage.setText("shareproparty");
+                    Toast.makeText(HomeActivity.this, "Your clicking on share button", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+//                    mTextMessage.setText(R.string.title_notifications);
+                    Toast.makeText(HomeActivity.this, "Nofitcations", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_search:
-                    mTextMessage.setText("Search");
+//                    mTextMessage.setText("Search");
+                    Toast.makeText(HomeActivity.this, "Searching proparty", Toast.LENGTH_SHORT).show();
                     return true;
             }
             return false;
--- app/src/main/java/com/example/mahdi/ah_rental/LoginActivity.java
+++ app/src/main/java/com/example/mahdi/ah_rental/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.mahdi.ah_rental;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.CharacterPickerDialog;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_auth =  findViewById(R.id.btn_auth_user);
+        btn_auth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
