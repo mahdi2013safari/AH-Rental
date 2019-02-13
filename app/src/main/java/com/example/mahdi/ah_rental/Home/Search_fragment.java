ee2c0402c37a198fef3b6fd85cc48e4d6b45b522
paypol-software-company <s.developer@paypol.af>
2019-02-13 09:40:21 +0430

    login page edited

--- app/src/main/java/com/example/mahdi/ah_rental/Home/Search_fragment.java
+++ app/src/main/java/com/example/mahdi/ah_rental/Home/Search_fragment.java
@@ -1,12 +1,15 @@
 package com.example.mahdi.ah_rental.Home;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.mahdi.ah_rental.MapsSearch;
 import com.example.mahdi.ah_rental.R;
 
 /**
@@ -15,6 +18,7 @@ import com.example.mahdi.ah_rental.R;
 public class Search_fragment extends Fragment {
 
 
+    View rootView;
     public Search_fragment() {
         // Required empty public constructor
     }
@@ -24,7 +28,16 @@ public class Search_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_fragment, container, false);
+        rootView = inflater.inflate(R.layout.fragment_search_fragment, container, false);
+        Button btn_search = rootView.findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGoogleMap = new Intent(getActivity(), MapsSearch.class);
+                startActivity(openGoogleMap);
+            }
+        });
+        return rootView;
     }
 
 }
