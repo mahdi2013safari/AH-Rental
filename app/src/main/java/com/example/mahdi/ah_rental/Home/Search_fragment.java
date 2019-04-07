3332e8a2965eee03e34911f45c8028fc258e33a8
paypol-software-company <s.developer@paypol.af>
2019-04-07 09:54:42 +0430

    edit activity

--- app/src/main/java/com/example/mahdi/ah_rental/Home/Search_fragment.java
+++ app/src/main/java/com/example/mahdi/ah_rental/Home/Search_fragment.java
@@ -8,9 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.example.mahdi.ah_rental.MapsSearch;
 import com.example.mahdi.ah_rental.R;
+import com.example.mahdi.ah_rental.location.GPSTracker;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +22,8 @@ import com.example.mahdi.ah_rental.R;
 public class Search_fragment extends Fragment {
 
 
+    int chooseLocation = 0 ;
+    Double latitude,longitude;
     View rootView;
     public Search_fragment() {
         // Required empty public constructor
@@ -30,6 +36,12 @@ public class Search_fragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_search_fragment, container, false);
         Button btn_search = rootView.findViewById(R.id.btn_search);
+        CheckBox close_to_me = rootView.findViewById(R.id.checkbox_close_to_me);
+        CheckBox any_location = rootView.findViewById(R.id.checkbox_any_location);
+        CheckBox location_name = rootView.findViewById(R.id.checkbox_location_name);
+
+
+
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +49,49 @@ public class Search_fragment extends Fragment {
                 startActivity(openGoogleMap);
             }
         });
+        close_to_me.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getActivity(), "check box close to me location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        any_location.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getActivity(), "checkbox any location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        location_name.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getActivity(), "check box location name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         return rootView;
     }
+
+    private void getLocation() {
+        if (chooseLocation == 0) {
+            //TODO: get location by GPS
+            GPSTracker myGps = new GPSTracker(getActivity());
+            myGps.getLocation();
+            if (myGps.canGetLocation) {
+                latitude = myGps.getLatitude();
+                longitude = myGps.getLongitude();
+            } else{
+                myGps.showSettingsAlert();
+            }
+        } else {
+            //TODO: get location by map
+        }
+    }// end of method getLocation()
 
 }
--- app/src/main/java/com/example/mahdi/ah_rental/location/GPSTracker.java
+++ app/src/main/java/com/example/mahdi/ah_rental/location/GPSTracker.java
@@ -2,16 +2,134 @@ package com.example.mahdi.ah_rental.location;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 
 public final class GPSTracker implements LocationListener {
 
+    private Context mContext;
+    public boolean isGpsEnabled = false;
+    boolean isNetworkEnabled = false;
+    public boolean canGetLocation = false;
+    Location location;
+    double latitude, longitude;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
+    private static final long MIN_TIME_BW_UPDATES = 1;
+    protected LocationManager locationManager;
 
+    public GPSTracker(Context mContext) {
+        this.mContext = mContext;
+        getLocation();
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+            isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isGpsEnabled){
+
+                canGetLocation=false;
+            }
+            else{
+                this.canGetLocation = true;
+                if (isGpsEnabled) {
+                    location = null;
+                    if (ActivityCompat.checkSelfPermission(mContext,
+                            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(mContext,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                if (isNetworkEnabled) {
+                    location = null;
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+    public double getLatitude(){
+        if(location !=null){
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+    public double getLongitude(){
+        if(location!=null){
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+    private boolean canGetLocation(){
+        return this.canGetLocation;
+    }
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+        alertDialog.setTitle("GPS Setting")
+                .setMessage("please Turn On Your GPS")
+                .setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        mContext.startActivities(new Intent[]{intent});
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
 
     @Override
     public void onLocationChanged(Location location) {
